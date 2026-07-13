05b0c5523631a1ef6aacd791c80228d457848fe2
sik2 <bickwang123@gmail.com>
2026-07-13 11:28:19 +0900

    9

--- infra/main.tf
+++ infra/main.tf
@@ -24,3 +24,16 @@ resource "aws_vpc" "vpc_1" {
     Name = "terra-vpc-1"
   }
 }
+
+
+# VPC 설정2 추가
+resource "aws_vpc" "vpc_2" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terra-vpc-2"
+  }
+}
